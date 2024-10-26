6817fa80a7cc9ab6fd53e93b39657ae96ce6b120
Prisha Bangera <prishasbangera@gmail.com>
2024-10-26 13:45:37 -0400

    Removed add method (not needed), moved to constructor

--- Button.js
+++ Button.js
@@ -1,8 +1,8 @@
+// Button object that contains a link to a p5 sketch
 class Button {
 
     constructor(text, color, link) {
 
-        this.text = text;
         var bu = document.createElement("button");
         this.buttonObject = bu;
         bu.style.backgroundColor = color;
@@ -11,12 +11,8 @@ class Button {
         bu.addEventListener("click", function() {
             document.getElementById("p5-project").src = link;
         });
-    }
-
-    add(el) {
 
         document.getElementById("buttons-container").appendChild(this.buttonObject);
-
 
     }
 
--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-
-
 var buttons = [
     new Button("Silly Drawing Scratchpad", "#63eb87", "https://editor.p5js.org/emeraldblackbird/full/Q_D4mpZ4U"),
     new Button("Would You Rather?", "#17939c", "https://editor.p5js.org/be.serious.bro/full/E2R24RYlK"),
     new Button("CIRCLS", "#e376cf", "https://editor.p5js.org/agm14/full/8h-QV8fNc")
 ];
-
-for (let b of buttons) {
-
-    b.add(document.getElementById("buttons-container"));
-
-}
