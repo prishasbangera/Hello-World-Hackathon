7fcbc8b0a42ca9ff8b99aea071b843495d2157f0
Prisha Bangera <prishasbangera@gmail.com>
2024-10-26 13:18:16 -0400

    added circls game

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 var buttons = [
     new Button("Silly Drawing Scratchpad", "#63eb87", "https://editor.p5js.org/emeraldblackbird/sketches/Q_D4mpZ4U"),
     new Button("Would You Rather?", "#17939c", "https://editor.p5js.org/be.serious.bro/sketches/E2R24RYlK"),
+    new Button("CIRCLS", "#e376cf", "https://editor.p5js.org/agm14/sketches/8h-QV8fNc")
 ];
 
 for (let b of buttons) {
