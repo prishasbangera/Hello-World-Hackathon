e0b331d4ab6e0ee0d6457671bbe46658ceeb7692
Prisha Bangera <prishasbangera@gmail.com>
2024-10-26 20:18:58 -0400

    Bear! and some css

--- Button.js
+++ Button.js
@@ -3,12 +3,14 @@ class Button {
 
     constructor(text, color, link) {
 
+        // Create the button and add styles
         var bu = document.createElement("button");
         this.buttonObject = bu;
         bu.classList.add("project-button")
         bu.style.backgroundColor = color;
         bu.textContent = text;
 
+        // On click, open the project in an iframe
         bu.addEventListener("click", function() {
 
             // remove previous active button
@@ -21,6 +23,7 @@ class Button {
 
         });
 
+        // add the button to the buttons-container div
         document.getElementById("buttons-container").appendChild(this.buttonObject);
 
     }
