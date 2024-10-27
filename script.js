bbf32bcd673ae29261599ba17b96f46361b2f8a7
Prisha Bangera <prishasbangera@gmail.com>
2024-10-26 21:37:45 -0400

    Changed Bear Sim link

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ var buttons = [
     new Button("Silly Drawing Scratchpad", "#63eb87", "https://editor.p5js.org/emeraldblackbird/full/Q_D4mpZ4U"),
     new Button("Would You Rather?", "#17939c", "https://editor.p5js.org/be.serious.bro/full/E2R24RYlK"),
     new Button("CIRCLES", "#e376cf", "https://editor.p5js.org/agm14/full/8h-QV8fNc"),
-    new Button("Bear Simulator", "#b4c447", "https://editor.p5js.org/balanannika/sketches/AS9e3e4LB")
+    new Button("Bear Simulator", "#b4c447", "https://editor.p5js.org/balanannika/full/AS9e3e4LB")
 ];
